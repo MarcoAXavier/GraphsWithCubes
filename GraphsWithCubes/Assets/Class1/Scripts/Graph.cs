9572641152adde597f238568121c51cf17fe4def
Marco Xavier <marcoxf96@gmail.com>
2021-12-06 08:59:48 -0300

    Graph mapping and points instantiating

--- GraphsWithCubes/Assets/Class1/Scripts/Graph.cs
+++ GraphsWithCubes/Assets/Class1/Scripts/Graph.cs
@@ -12,15 +12,25 @@ public class Graph : MonoBehaviour
     private float _resolution = 10;
     private float Resolution => _resolution;
 
+    [SerializeField, Range(1,10)]
+    private float _pointsSizeDivider;
+    private float PointsSizeDivider => _pointsSizeDivider;
 
-    private void Awake()
+
+    private void Awake ()=> PositionAndScalePoints();
+    
+
+    private void PositionAndScalePoints()
     {
         for (int i = 0; i < Resolution; i++)
         {
-            Vector3 pos = Vector3.right * i / (PointPrefab.transform.localScale.x*2);
-            pos.y = pos.x*pos.x;
-            Instantiate(PointPrefab, pos, Quaternion.identity);
+            Vector3 pos = Vector3.right * (i / PointsSizeDivider - 1f);
+
+            Transform point = Instantiate(PointPrefab);
+            //pos.y = pos.x*pos.x;
+
+            point.localPosition = pos;
+            point.localScale = Vector3.one / PointsSizeDivider;
         }
     }
-
 }
