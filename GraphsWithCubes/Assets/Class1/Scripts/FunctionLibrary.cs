342a3b0662c19c4e750a7f83b0edbc616ffd2da1
Marco Xavier <marcoxf96@gmail.com>
2022-01-19 17:30:27 -0300

    Adds Ripple function

--- GraphsWithCubes/Assets/Class1/Scripts/FunctionLibrary.cs
+++ GraphsWithCubes/Assets/Class1/Scripts/FunctionLibrary.cs
@@ -1,8 +1,23 @@
-using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
+    public enum Functions { Wave = 0, DoubleWave = 1, Ripple = 2 }
+    
+    public static float GetFunction(Functions functionIndex, float x, float t)
+    {
+        switch ((int)functionIndex)
+        {
+            case 0:
+                return Wave(x, t);
+            case 1:
+                return DoubleWave(x, t);
+            case 2:
+                return Ripple(x, t);
+            default:
+                return float.PositiveInfinity;
+        }
+    } 
     public static float Wave(float x, float t)
     {
         return Sin(PI * (x + t));
@@ -13,5 +28,11 @@ public static class FunctionLibrary
         float y =  Sin(PI * (x + t));
         y += 0.5f * Sin(2f * PI * (x + t));
         return y * (2f/3f);
+    }
+    public static float Ripple(float x, float t)
+    {
+        float distance = Abs(x);
+        float y = Sin(PI*(4 * distance-t))   /   (1f+(10f*distance));
+        return y;
     }
 }
